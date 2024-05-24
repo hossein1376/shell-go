485feb96cc4c15676eb761b07b1a21c20bd0b68f
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2024-05-24 10:22:00 +0330

    Command not found

--- cmd/shell/main.go
+++ cmd/shell/main.go
@@ -4,11 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	line, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Printf("reading line: %v\n", err)
+		return
+	}
+	line = strings.TrimSpace(line)
+
+	fmt.Printf("%s: command not found\n", line)
 }
