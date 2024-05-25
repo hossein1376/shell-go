e7da185e9b949fc23a39e464d030f8bd0e73c097
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2024-05-25 10:07:00 +0330

    REPL

--- cmd/shell/main.go
+++ cmd/shell/main.go
@@ -8,15 +8,17 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	line, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Printf("reading line: %v\n", err)
-		return
+		// Wait for user input
+		line, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Printf("reading line: %v\n", err)
+			return
+		}
+		line = strings.TrimSpace(line)
+
+		fmt.Printf("%s: command not found\n", line)
 	}
-	line = strings.TrimSpace(line)
-
-	fmt.Printf("%s: command not found\n", line)
 }
