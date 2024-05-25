982ac0530718400d615d9dae06d20bb39b822e9e
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2024-05-25 10:12:00 +0330

    Exit command

--- cmd/shell/main.go
+++ cmd/shell/main.go
@@ -4,7 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+)
+
+const (
+	Exit = "exit"
 )
 
 func main() {
@@ -17,8 +22,26 @@ func main() {
 			fmt.Printf("reading line: %v\n", err)
 			return
 		}
-		line = strings.TrimSpace(line)
 
-		fmt.Printf("%s: command not found\n", line)
+		resp := evaluate(strings.TrimSpace(line))
+		fmt.Println(resp)
 	}
+}
+
+func evaluate(line string) string {
+	switch {
+	case strings.HasPrefix(line, Exit):
+		status := 0
+		if p := strings.Split(line, " "); len(p) == 2 {
+			var err error
+			status, err = strconv.Atoi(p[1])
+			if err != nil || status < 0 || status > 255 {
+				return fmt.Sprintf("invalid exit status: %s", p[1])
+			}
+		}
+		os.Exit(status)
+	default:
+		return fmt.Sprintf("%s: command not found", line)
+	}
+	return ""
 }
